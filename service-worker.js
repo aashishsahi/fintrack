a3c1e5b7567e0069eeba27d6bc79dc0a89e7e7a9
aashishsahi <32257048+aashishsahi@users.noreply.github.com>
2026-05-31 13:53:10 +0530

    new version release bugfix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'fintrack-cache-v2';
+const CACHE_NAME = 'fintrack-cache-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -14,6 +14,17 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
   );
 });
 
